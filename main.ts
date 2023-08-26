f168cd740860e3b9d4ddc7324c44104ecb399d85
chanma <yunosuke.homma@icloud.com>
2023-08-26 13:04:13 +0900

    add some moving func

--- main.ts
+++ main.ts
@@ -1,27 +1,86 @@
-// まずは、Canvas要素と2Dコンテキストを取得
+const gameState = {
+  ballX: 50,
+  ballY: 50,
+};
+
+let speedX = 2;
+let speedY = 2;
+
+/* 自動で動く */
+/* 何も書かなければ動かない */
+function update(): void {
+  // gameState.ballX += speedX;
+  // gameState.ballY += speedY;
+
+  // // 画面の端に到達したら反対方向へ
+  // if (gameState.ballX <= 0 || gameState.ballX >= canvas.width) {
+  //   speedX = -speedX;
+  // }
+  // if (gameState.ballY <= 0 || gameState.ballY >= canvas.height) {
+  //   speedY = -speedY;
+  // }
+}
+
+/* keyが入力されたときの動き */
+document.addEventListener("keydown", function(event) {
+  /* 入力しなくても動く */
+  // if (event.key === "ArrowUp") {
+  //   speedY = -Math.abs(speedY);  // 上に移動
+  // }
+  // if (event.key === "ArrowDown") {
+  //   speedY = Math.abs(speedY);  // 下に移動
+  // }
+  // if (event.key === "ArrowLeft") {
+  //   speedX = -Math.abs(speedX);  // 左に移動
+  // }
+  // if (event.key === "ArrowRight") {
+  //   speedX = Math.abs(speedX);  // 右に移動
+  // }
+/* 入力しない限り動かない */
+// 画面の端でない限り動く
+  const moveAmount = 10;
+  if (event.key === "ArrowUp" && gameState.ballY > 0) {
+    gameState.ballY -= moveAmount;
+  }
+  if (event.key === "ArrowDown" && gameState.ballY < canvas.height) {
+    gameState.ballY += moveAmount;
+  }
+  if (event.key === "ArrowLeft" && gameState.ballX > 0) {
+    gameState.ballX -= moveAmount;
+  }
+  if (event.key === "ArrowRight" && gameState.ballX < canvas.width) {
+    gameState.ballX += moveAmount;
+  }
+});
+
+
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 
-// ゲームの変数を初期化
-let ballX = 50;
-let ballY = 50;
+/* カーソルに合わせた動き */
+// canvas.addEventListener("mousemove", function(event) {
+//   const rect = canvas.getBoundingClientRect();
+//   const x = event.clientX - rect.left;
+//   const y = event.clientY - rect.top;
 
-function draw() {
-  // 画面をクリア
-  ctx.fillStyle = "#000000"; // 黒
+//   gameState.ballX = x;
+//   gameState.ballY = y;
+// });
+
+function draw(): void {
+  ctx.fillStyle = "#000000";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // ボールを描画
-  ctx.fillStyle = "#FFFFFF"; // 白
+  ctx.fillStyle = "#FFFFFF";
   ctx.beginPath();
-  ctx.arc(ballX, ballY, 10, 0, Math.PI * 2);
+  ctx.arc(gameState.ballX, gameState.ballY, 10, 0, Math.PI * 2);
   ctx.fill();
 }
 
-function gameLoop() {
+function gameLoop(): void {
+  update();
   draw();
   requestAnimationFrame(gameLoop);
 }
 
-// ゲームループを開始
 gameLoop();
